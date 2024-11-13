6897f32524f0a52e40b386ecd4dbf8cdabb1b58f
Efimish <efim.ishenin@gmail.com>
2024-11-13 23:12:41 +0700

    added camera query

--- src/pages/Codes.tsx
+++ src/pages/Codes.tsx
@@ -2,6 +2,25 @@ import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonList, IonPage
 import { add, logoGithub, logoGoogle, logoApple, helpOutline } from 'ionicons/icons';
 import Code from '../components/Code';
 
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+const takePicture = async () => {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  });
+
+  // image.webPath will contain a path that can be set as an image src.
+  // You can access the original file using image.path, which can be
+  // passed to the Filesystem API to read the raw data of the image,
+  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+  const imageUrl = image.webPath;
+
+  // Can be set to the src of an image now
+  alert(imageUrl);
+};
+
 const Codes: React.FC = () => {
   return (
     <IonPage>
@@ -15,7 +34,7 @@ const Codes: React.FC = () => {
           <IonTitle>Codes</IonTitle>
 
           <IonButtons slot="end">
-            <IonButton>
+            <IonButton onClick={() => takePicture()}>
               <IonIcon slot="icon-only" icon={add}></IonIcon>
             </IonButton>
           </IonButtons>
